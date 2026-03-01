ec907a330aaa5ee717f01e1a3348908d0f5ebb63
TechBugFinder <techbugfinder007@gmail.com>
2026-03-01 09:12:31 +0530

    new code added

--- Sum.java
+++ Sum.java
@@ -4,6 +4,10 @@ public class Sum {
         int b = 10;
         int c = a + b;
         System.out.println("The sum is: " + c);
+    
+        System.out.println("Hi new code added ");
+
+    
     }
 }
 
